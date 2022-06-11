bd3cbc40aa058bb18a24537d2ba9bd837bb7a34f
pranavundre <90198260+pranavundre@users.noreply.github.com>
2022-06-11 09:51:11 +0530

    Time: 76 ms (8.47%), Space: 10.1 MB (75.71%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.c
+++ 566-reshape-the-matrix/566-reshape-the-matrix.c
@@ -7,17 +7,11 @@
  */
 int** matrixReshape(int** mat, int matSize, int* matColSize, int r, int c, int* returnSize, int** returnColumnSizes){
     
-    // int num_rows = sizeof(mat) / sizeof(mat[0]);
-    // int num_cols = sizeof(mat[0]) / sizeof(mat[0][0]);
-    // int newMat[r][c];
-    // int **newMat = (int *)malloc(r * c * sizeof(int));
-    
     if(r*c != matSize*(*matColSize)){
         *returnSize = matSize;
         *returnColumnSizes = matColSize;
         return mat;
     }
-    
     
     *returnColumnSizes = malloc(sizeof(int) * r);
     int** newMat = malloc(sizeof(int*) * r);
@@ -37,13 +31,11 @@ int** matrixReshape(int** mat, int matSize, int* matColSize, int r, int c, int* 
     
     int m = 0;
     
-    for(int i = 0; i < r; i++){
-        // *newMat[i] = (int*)malloc(c * sizeof(int));
+    for(int i = 0; i < r; i++)
         for(int j = 0; j < c; j++){
             newMat[i][j] = row[m];
             m++;
         }
-    }
     
     **returnColumnSizes = c;
     *returnSize = r;
