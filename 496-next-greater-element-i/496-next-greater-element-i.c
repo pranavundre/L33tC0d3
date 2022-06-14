b0b261ea7a688323c2c0848c6f1558a0c38127ca
pranavundre <90198260+pranavundre@users.noreply.github.com>
2022-06-14 22:24:17 +0530

    Time: 21 ms (33.94%), Space: 6.4 MB (68.35%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.c
+++ 496-next-greater-element-i/496-next-greater-element-i.c
@@ -9,18 +9,17 @@ int* nextGreaterElement(int* nums1, int nums1Size, int* nums2, int nums2Size, in
     *returnSize = nums1Size;
     for(int i = 0; i < nums1Size; i++){
         returnArr[i] = -1;
-        for(int j = 0; j < nums2Size; j++){
+        for(int j = 0; j < nums2Size; j++)
             if(nums1[i] == nums2[j]){
-                int x = nums1[i], k = j+1;
+                int k = j+1;
                 while(k < nums2Size){
-                    if(x < nums2[k]){
+                    if(nums1[i] < nums2[k]){
                         returnArr[i] = nums2[k];
                         break;
                     }
                     k++;
                 }
             }
-        }
     }
     return returnArr;
 }
