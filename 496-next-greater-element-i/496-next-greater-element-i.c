cc08df1906cfba2fb7e16f42c350b860a3c0784f
pranavundre <90198260+pranavundre@users.noreply.github.com>
2022-06-14 22:22:41 +0530

    Time: 24 ms (27.06%), Space: 6.3 MB (68.35%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.c
@@ -0,0 +1,26 @@
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* nextGreaterElement(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize){
+    
+    int *returnArr = (int *)malloc(nums1Size * sizeof(int));
+    *returnSize = nums1Size;
+    for(int i = 0; i < nums1Size; i++){
+        returnArr[i] = -1;
+        for(int j = 0; j < nums2Size; j++){
+            if(nums1[i] == nums2[j]){
+                int x = nums1[i], k = j+1;
+                while(k < nums2Size){
+                    if(x < nums2[k]){
+                        returnArr[i] = nums2[k];
+                        break;
+                    }
+                    k++;
+                }
+            }
+        }
+    }
+    return returnArr;
+}
