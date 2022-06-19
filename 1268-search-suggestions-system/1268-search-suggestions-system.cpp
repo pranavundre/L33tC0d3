050bbfa05e1f3304ed949ced30de3ba705df7e9c
pranavundre <90198260+pranavundre@users.noreply.github.com>
2022-06-19 11:15:44 +0530

    Time: 163 ms (52.80%), Space: 26.4 MB (81.60%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -2,24 +2,15 @@ class Solution {
 public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
         
-        int n = searchWord.length();
-        vector<vector<string>> ret;
-        // ret.reserve(n);
-        
+        vector<vector<string>> ret;        
         sort(products.begin(), products.end());
-        
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < searchWord.length(); i++){
             vector<string> temp;
-            int c = 0;
-            for(int j = 0; j < products.size(); j++){
-                if((products[j].compare(0, i+1, searchWord, 0, i+1))==0 && c!=3){
+            for(int j = 0; j < products.size(); j++)
+                if((products[j].compare(0, i+1, searchWord, 0, i+1))==0 && temp.size()!=3)
                     temp.push_back(products[j]);
-                    c++;
-                }
-            }
             ret.push_back(temp);
         }
-        
         return ret;
     }
 };
