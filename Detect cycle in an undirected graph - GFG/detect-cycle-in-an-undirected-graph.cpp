c9d36543be3e7fa6ccb704776c2fd13de6dd1170
Pranav Undre <undrepranav@gmail.com>
2023-05-20 10:54:11 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -3,30 +3,36 @@
 using namespace std;
 
 // } Driver Code Ends
-
 class Solution {
   public:
-    bool checkBFS(int index, vector<int>& vis, vector<int> adj[], int& V){
-        queue<int> q;
-        q.emplace(index);
-        while(!q.empty()){
-            int temp = q.front(), visCount = 0;
-            q.pop();
-            for(int i : adj[temp])
-                if(vis[i]++ > 0) visCount++;
-                else q.emplace(i);
-            if(visCount > 1) return true;
+    // Function to detect cycle in an undirected graph.
+    void checkDFS(vector<int> adj[], vector<int>& vis, int &index, bool &loop){
+        int visCount = 0;
+        for(int i : adj[index]){
+            if(vis[i] != 0) visCount++;
         }
-        return false;
+        if(visCount > 1){
+            loop = true;
+            return;
+        }
+        for(int i : adj[index]){
+            if(vis[i]++ == 0){
+                checkDFS(adj, vis, i, loop);
+            }
+        }
     }
     bool isCycle(int V, vector<int> adj[]) {
         vector<int> vis(V, 0);
-        for(int i = 0; i < V; i++)
-            if(vis[i]++ == 0 && checkBFS(i, vis, adj, V)) return true;
+        for(int i = 0; i < V; i++){
+            if(vis[i]++ == 0){
+                bool loop = false;
+                checkDFS(adj, vis, i, loop);
+                if(loop) return true;
+            }
+        }
         return false;
     }
 };
-
 
 //{ Driver Code Starts.
 int main() {
