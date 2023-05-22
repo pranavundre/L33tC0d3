64693cdad45c8827e6b20461a721e42de00a470e
Pranav Undre <undrepranav@gmail.com>
2023-05-22 09:09:29 +0530

    Added solution - LeetHub

--- Eventual Safe States - GFG/eventual-safe-states.cpp
+++ Eventual Safe States - GFG/eventual-safe-states.cpp
@@ -25,12 +25,7 @@ public:
     }
     vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
         vector<int> safeNodes(V, 0), vis(V, 0), res;
-        for(int i = 0; i < V; i++){
-            if(adj[i].empty()){
-                safeNodes[i] = vis[i] = 1;
-            }
-        }
-        
+        for(int i = 0; i < V; i++) if(adj[i].empty()) safeNodes[i] = vis[i] = 1;
         for(int i = 0; i < V; i++){
             if(safeNodes[i] == 1) res.emplace_back(i);
             if(vis[i] == 0 && checkDFS(adj, vis, safeNodes, i)){
