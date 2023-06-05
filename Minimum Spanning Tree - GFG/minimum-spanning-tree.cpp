e51b9a5d796d780a035186ddc3d82cb9520065ce
Pranav Undre <undrepranav@gmail.com>
2023-06-05 08:57:26 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,39 +3,33 @@
 using namespace std;
 
 // } Driver Code Ends
+
 class Solution{
 	public:
     int spanningTree(int V, vector<vector<int>> adj[]){
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         vector<int> vis(V, 0);
-        // vis[0] = 1;
-        --V;
-        
+
         int minSum = 0;
-        
         pq.push({0, 0});
         
         while(!pq.empty()){
             auto p = pq.top();
             pq.pop();
+            
             int d = p.first, node = p.second;
-            if(vis[node] == 1){
-                continue;
-                // V--;
-            }
+            
+            if(vis[node] == 1) continue;
+            
             vis[node]++;
             minSum += d;
-            // if(V==0) break;
-            for(auto vec : adj[node]){
-                if(vis[vec[0]] == 0){
-                    pq.push({vec[1], vec[0]});
-                }
-            }
+            
+            for(auto vec : adj[node]) if(vis[vec[0]] == 0) pq.push({vec[1], vec[0]});
         }
-        
         return minSum;
     }
 };
+
 
 //{ Driver Code Starts.
 
